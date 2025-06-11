936712fa5bd84f87ad09ed80b342d3472664a96e
Veilcraft-oday <o77853985@gmail.com>
2025-06-11 14:56:42 +0200

    Update script.js

--- assets/script.js
+++ assets/script.js
@@ -15,7 +15,7 @@ document.addEventListener("DOMContentLoaded", function () {
       document.getElementById("riddle-placeholder").innerHTML = `
   <div id="riddle">
     <p>Decode the memory. Say the name.</p>
-     <!-- sayhisname:c2F5b2RheQ== -->
+     <p> sayhisname:c2F5b2RheQ== </p>
     <input type="text" id="answer" placeholder="Enter your answer">
     <button onclick="checkAnswer()">Submit</button>
   </div>
